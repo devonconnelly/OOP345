3c78f6d9530c2125047ffc5f7cd28ce10d630f8e
Devon Connelly <122404957+devonconnelly@users.noreply.github.com>
2023-09-20 15:52:10 -0400

    part 1

---
+++ WS02/part 1/AirportModels.h
@@ -0,0 +1,40 @@
+#ifndef SDDS_AIRPORTMODELS_H
+#define SDDS_AIRPORTMODELS_H
+namespace sdds {
+class Airport {
+    char* m_code = nullptr;
+    char* m_name = nullptr;
+    char* m_city = nullptr;
+    char* m_state = nullptr;
+    char* m_country = nullptr;
+    float m_latitude{};
+    float m_longitude{};
+    
+public:
+    void display() const;
+    Airport& operator=(const Airport& airport);
+    Airport();
+    Airport(const char* code, const char* name, const char* city, const char* state, const char* country, const float latitude, const float longitude);
+    char* getCountry() const;
+    char* getState() const;
+    Airport& set(const char* code, const char* name, const char* city, const char* state, const char* country, const float latitude, const float longitude);
+};
+
+std::ostream& operator<<(std::ostream& ostr, const Airport& airport);
+void copy(char*& a, const char* b);
+
+class AirportLog {
+    Airport* m_airports = nullptr;
+    size_t m_size{};
+public:
+    AirportLog();
+    AirportLog(const char* filename);
+    void addAirport(const Airport& airport);
+    AirportLog findAirport(const char* state, const char* country);
+    Airport operator[](size_t index) const;
+    operator size_t() const;
+};
+}
+
+
+#endif /* AirportModels_h */

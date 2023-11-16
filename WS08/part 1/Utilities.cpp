87b8e85e4af03b4d362fe030baf39b572c54f18e
Devon Connelly <122404957+devonconnelly@users.noreply.github.com>
2023-11-16 12:44:25 -0500

    test

---
+++ WS08/part 1/Utilities.cpp
@@ -0,0 +1,42 @@
+// Workshop 8 - Smart Pointers
+// 2019/11 - Cornel
+
+#include <memory>
+#include <utility>
+#include "DataBase.h"
+#include "Profile.h"
+#include "Utilities.h"
+
+using namespace std;
+
+namespace sdds {
+	DataBase<Profile> excludeRaw(const DataBase<Profile>& allProfiles, const DataBase<Profile>& bannedProfiles) {
+		DataBase<Profile> result;
+		// TODO: Add your code here to build a collection of Profiles.
+		//         The result should contain only profiles from `allProfiles`
+		//         which are not in `bannedProfiles` using Raw Pointers.
+        for(size_t i = 0; i < allProfiles.size(); ++i) {
+            bool isBanned = false;
+
+            for(size_t j = 0; j < bannedProfiles.size(); ++j) {
+                if(allProfiles[i].m_age == bannedProfiles[j].m_age &&
+                    allProfiles[i].m_name.first_name == bannedProfiles[j].m_name.first_name &&
+                    allProfiles[i].m_name.last_name == bannedProfiles[j].m_name.last_name) {
+                    isBanned = true;
+                    j = bannedProfiles.size();
+                }
+            }
+            if(!isBanned) {
+                Profile* prof = new Profile(allProfiles[i]);
+                try {
+                    prof->validateAddress();
+                    result += *prof;
+                } catch(...) {
+                    
+                }
+                delete prof;
+            }
+        }
+        return result;
+	}
+}

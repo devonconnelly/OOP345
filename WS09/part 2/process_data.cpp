765c9024af5cafb9aa04e9bcf9d97cde1ed63ac2
Devon Connelly <122404957+devonconnelly@users.noreply.github.com>
2023-11-23 12:52:02 -0500

    p2

--- WS09/part 2/process_data.cpp
+++ WS09/part 2/process_data.cpp
@@ -3,6 +3,11 @@
 // 2021/1/5 - Jeevan Pant
 // 2023/11/17 - Cornel
 
+// Devon Connelly
+// 105322218
+// dconnelly@myseneca.ca
+// Nov. 23 2023
+
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -13,12 +18,12 @@
 
 namespace sdds
 {
-	// The following function receives array (pointer) as the first argument, number of array 
-	//   elements (size) as second argument, divisor as the third argument, and avg as fourth argument. 
-	//   size and divisor are not necessarily same. When size and divisor hold same value, avg will 
-	//   hold average of the array elements. When they are different, avg will hold a value called 
-	// 	 as average-factor. For part 1, you will be using same value for size and double. Use of 
-	//   different values for size and divisor will be useful for multi-threaded implementation in part-2. 
+	// The following function receives array (pointer) as the first argument, number of array
+	//   elements (size) as second argument, divisor as the third argument, and avg as fourth argument.
+	//   size and divisor are not necessarily same. When size and divisor hold same value, avg will
+	//   hold average of the array elements. When they are different, avg will hold a value called
+	// 	 as average-factor. For part 1, you will be using same value for size and double. Use of
+	//   different values for size and divisor will be useful for multi-threaded implementation in part-2.
 	void computeAvgFactor(const int* arr, int size, int divisor, double& avg) {
 		avg = 0;
 		for (int i = 0; i < size; i++) {
@@ -27,13 +32,13 @@ namespace sdds
 		avg /= divisor;
 	}
 
-	// The following function receives array (pointer) as the first argument, number of array elements  
+	// The following function receives array (pointer) as the first argument, number of array elements
 	//   (size) as second argument, divisor as the third argument, computed average value of the data items
-	//   as fourth argument, and var as fifth argument. Size and divisor are not necessarily same as in the 
-	//   case of computeAvgFactor. When size and divisor hold same value, var will get total variance of 
-	//   the array elements. When they are different, var will hold a value called as variance factor. 
-	//   For part 1, you will be using same value for size and double. Use of different values for size 
-	//   and divisor will be useful for multi-threaded implementation in part-2. 
+	//   as fourth argument, and var as fifth argument. Size and divisor are not necessarily same as in the
+	//   case of computeAvgFactor. When size and divisor hold same value, var will get total variance of
+	//   the array elements. When they are different, var will hold a value called as variance factor.
+	//   For part 1, you will be using same value for size and double. Use of different values for size
+	//   and divisor will be useful for multi-threaded implementation in part-2.
 	void computeVarFactor(const int* arr, int size, int divisor, double avg, double& var) {
 		var = 0;
 		for (int i = 0; i < size; i++) {
@@ -42,68 +47,100 @@ namespace sdds
 		var /= divisor;
 	}
 
-	// The following constructor of the functor receives name of the data file, opens it in 
-	//   binary mode for reading, reads first int data as total_items, allocate memory space 
-	//   to hold the data items, and reads the data items into the allocated memory space. 
-	//   It prints first five data items and the last three data items as data samples. 
+	// The following constructor of the functor receives name of the data file, opens it in
+	//   binary mode for reading, reads first int data as total_items, allocate memory space
+	//   to hold the data items, and reads the data items into the allocated memory space.
+	//   It prints first five data items and the last three data items as data samples.
 	//   
-	ProcessData::ProcessData(const std::string& filename, int n_threads) {  
+	ProcessData::ProcessData(const std::string& filename, int n_threads) {
 		// TODO: Open the file whose name was received as parameter and read the content
 		//         into variables "total_items" and "data". Don't forget to allocate
 		//         memory for "data".
 		//       The file is binary and has the format described in the specs.
-
-
-
-
-
+        std::ifstream f(filename, std::ios::in | std::ios::binary);
+        f.read(reinterpret_cast<char*>(&total_items), sizeof(total_items));
+        data = new int[total_items];
+        for(int i = 0; i < total_items; i++) {
+            f.read(reinterpret_cast<char*>(&data[i]), sizeof(int));
+        }
 
 		std::cout << "Item's count in file '"<< filename << "': " << total_items << std::endl;
 		std::cout << "  [" << data[0] << ", " << data[1] << ", " << data[2] << ", ... , "
 		          << data[total_items - 3] << ", " << data[total_items - 2] << ", "
 		          << data[total_items - 1] << "]" << std::endl;
-
-		// Following statements initialize the variables added for multi-threaded 
-		//   computation
-		num_threads = n_threads; 
-		averages = new double[num_threads] {};
-		variances = new double[num_threads] {};
-		p_indices = new int[num_threads+1] {};
-		for (int i = 0; i < num_threads; i++)
-			p_indices[i] = i * (total_items / num_threads);
-		p_indices[num_threads] = total_items;
+        
+        // Following statements initialize the variables added for multi-threaded
+        //   computation
+        num_threads = n_threads;
+        averages = new double[num_threads] {};
+        variances = new double[num_threads] {};
+        p_indices = new int[num_threads+1] {};
+        for (int i = 0; i < num_threads; i++)
+            p_indices[i] = i * (total_items / num_threads);
+        p_indices[num_threads] = total_items;
 	}
+
 	ProcessData::~ProcessData() {
 		delete[] data;
-		delete[] averages;
-		delete[] variances;
-		delete[] p_indices;
+        delete[] averages;
+        delete[] variances;
+        delete[] p_indices;
 	}
 
 	ProcessData::operator bool() const {
-		return total_items > 0 && data;
+		return total_items > 0 && data != nullptr;
 	}
 
-	// TODO Improve operator() function from part-1 for multi-threaded operation. Enhance the  
-	//   function logic for the computation of average and variance by running the 
-	//   function computeAvgFactor and computeVarFactor in multile threads. 
-	// The function divides the data into a number of parts, where the number of parts is 
-	//   equal to the number of threads. Use multi-threading to compute average-factor for 
-	//   each part of the data by calling the function computeAvgFactor. Add the obtained 
-	//   average-factors to compute total average. Use the resulting total average as the 
-	//   average value argument for the function computeVarFactor, to compute variance-factors 
-	//   for each part of the data. Use multi-threading to compute variance-factor for each 
-	//   part of the data. Add computed variance-factors to obtain total variance.
-	// Save the data into a file with filename held by the argument fname_target. 
-	// Also, read the workshop instruction.
-
-
-
-
-
-
-
-
-
+    // TODO Improve operator() function from part-1 for multi-threaded operation. Enhance the
+    //   function logic for the computation of average and variance by running the
+    //   function computeAvgFactor and computeVarFactor in multile threads.
+    // The function divides the data into a number of parts, where the number of parts is
+    //   equal to the number of threads. Use multi-threading to compute average-factor for
+    //   each part of the data by calling the function computeAvgFactor. Add the obtained
+    //   average-factors to compute total average. Use the resulting total average as the
+    //   average value argument for the function computeVarFactor, to compute variance-factors
+    //   part of the data. Add computed variance-factors to obtain total variance.
+//   for each part of the data. Use multi-threading to compute variance-factor for each
+    // Save the data into a file with filename held by the argument fname_target.
+    // Also, read the workshop instruction.
+    int ProcessData::operator()(const std::string& target_file, double& avg, double& var) {
+        
+        std::vector<std::thread> avgThreads;
+        for (int i = 0; i < num_threads; ++i) {
+            auto bound = std::bind(computeAvgFactor, data + p_indices[i], p_indices[i + 1] - p_indices[i], total_items, std::ref(averages[i]));
+            avgThreads.push_back(std::thread (bound));
+        }
+        for (auto& thread : avgThreads) {
+            thread.join();
+        }
+        for(int i = 0; i < num_threads; i++) {
+            avg += averages[i];
+        }
+        std::vector<std::thread> varThreads;
+        for (int i = 0; i < num_threads; ++i) {
+            
+            auto bound = std::bind(computeVarFactor, data + p_indices[i], p_indices[i + 1] - p_indices[i], total_items, avg, std::ref(variances[i]));
+            varThreads.push_back(std::thread (bound));
+        }
+        for (auto& thread : varThreads) {
+            thread.join();
+        }
+        for(int i = 0; i < num_threads; i++) {
+            var += variances[i];
+        }
+        
+        std::ofstream f(target_file, std::ios::out | std::ios::binary | std::ios::trunc);
+        if(f) {
+            f.write(reinterpret_cast<char*>(&total_items), sizeof(int));
+            for(int i = 0; i < total_items; i++) {
+                f.write(reinterpret_cast<char*>(&data[i]), sizeof(int));
+            }
+        }
+        else {
+            throw "Cannot open file";
+        }
+        return avg;
+        
+    }
 
 }
--- WS09/part 2/process_data.h
+++ WS09/part 2/process_data.h
@@ -9,26 +9,26 @@
 #include<string>
 
 namespace sdds {
-	void computeAvgFactor(const int* arr, int size, int divisor, double& avg);
-	void computeVarFactor(const int* arr, int size, int divisor, double avg, double& var);
+    void computeAvgFactor(const int* arr, int size, int divisor, double& avg);
+    void computeVarFactor(const int* arr, int size, int divisor, double avg, double& var);
 
-	class ProcessData {
-		int total_items{};
-		int* data{};
-		// Following parameters are included to be used for managing multi-threaded 
-		//  computation in the operator() function. 
-		int num_threads{ 0 }; // to hold number of threads 
-		double* averages{ nullptr }; // to hold average factors 
-		double* variances{ nullptr }; // to hold variance factors
-		int* p_indices{ nullptr }; // to hold partitioning indices
-	public:
-		ProcessData(const std::string& input_file, int n_threads);
-		ProcessData(const ProcessData&) = delete;
-		ProcessData& operator=(const ProcessData&) = delete;
-		~ProcessData();
-		operator bool() const;
-		int operator()(const std::string& target_file, double& avg, double& var);
-	};
+    class ProcessData {
+        int total_items{};
+        int* data{};
+        // Following parameters are included to be used for managing multi-threaded
+        //  computation in the operator() function.
+        int num_threads{ 0 }; // to hold number of threads
+        double* averages{ nullptr }; // to hold average factors
+        double* variances{ nullptr }; // to hold variance factors
+        int* p_indices{ nullptr }; // to hold partitioning indices
+    public:
+        ProcessData(const std::string& input_file, int n_threads);
+        ProcessData(const ProcessData&) = delete;
+        ProcessData& operator=(const ProcessData&) = delete;
+        ~ProcessData();
+        operator bool() const;
+        int operator()(const std::string& target_file, double& avg, double& var);
+    };
 }
 
 #endif
